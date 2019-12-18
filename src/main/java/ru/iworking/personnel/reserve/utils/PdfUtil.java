6118c6047d56e6fda7c591fa85e6a58734af4e09
Земцов Алексей <zemtsov1997@yandex.ru>
2019-12-18 19:45:31 +0300

    Create image in pdf document

--- src/main/java/ru/iworking/personnel/reserve/utils/PdfUtil.java
+++ src/main/java/ru/iworking/personnel/reserve/utils/PdfUtil.java
@@ -3,12 +3,15 @@ package ru.iworking.personnel.reserve.utils;
 import com.itextpdf.io.font.FontProgram;
 import com.itextpdf.io.font.FontProgramFactory;
 import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.property.TextAlignment;
 import org.apache.commons.io.IOUtils;
@@ -35,6 +38,15 @@ public class PdfUtil {
 
         //Add paragraph to the document
         document.add(new Paragraph("Резюме").setFont(pdfFont).setTextAlignment(TextAlignment.CENTER));
+
+        if (resume.getPhoto() != null) {
+            ImageData data = ImageDataFactory.create(resume.getPhoto());
+            // Creating an Image object
+            Image image = new Image(data);
+            image.scaleToFit(200F, 200F);
+            document.add(image);
+        }
+
         document.add(new Paragraph("Ф.И.О.: "+
                 resume.getLastName() + " " +
                 resume.getFirstName() + " " +
@@ -43,10 +55,26 @@ public class PdfUtil {
         document.add(new Paragraph("тел.: "+resume.getNumberPhone()).setFont(pdfFont));
         document.add(new Paragraph("емайл: "+resume.getEmail()).setFont(pdfFont));
         document.add(new Paragraph("профессия: "+resume.getProfession()).setFont(pdfFont));
-        document.add(new Paragraph("зарплата: "+resume.getWage().toString() + " " + resume.getCurrency().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
-        document.add(new Paragraph("профобласть: "+resume.getProfField().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
-        document.add(new Paragraph("график: "+resume.getWorkType().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
-        document.add(new Paragraph("образование: "+resume.getEducation().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
+        if (resume.getWage() != null) {
+            document.add(new Paragraph("зарплата: "+resume.getWage().toString() + " " + resume.getCurrency().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
+        } else {
+            document.add(new Paragraph("зарплата: не указана").setFont(pdfFont));
+        }
+        if (resume.getProfField() != null) {
+            document.add(new Paragraph("профобласть: "+resume.getProfField().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
+        } else {
+            document.add(new Paragraph("профобласть: не указана").setFont(pdfFont));
+        }
+        if (resume.getWorkType() != null) {
+            document.add(new Paragraph("график: "+resume.getWorkType().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
+        } else {
+            document.add(new Paragraph("график: не указан").setFont(pdfFont));
+        }
+        if (resume.getEducation() != null) {
+            document.add(new Paragraph("образование: "+resume.getEducation().getNameToView(LocaleUtils.getDefault())).setFont(pdfFont));
+        } else {
+            document.add(new Paragraph("образование: не указано").setFont(pdfFont));
+        }
 
         //Close document
         document.close();
