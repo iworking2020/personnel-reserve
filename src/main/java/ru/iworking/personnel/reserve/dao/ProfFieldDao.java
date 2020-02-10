380b69798f05015fdda8f88ff9110f8b941f88f2
Алексей Земцов <azemtsov@diasoft.ru>
2020-02-10 14:02:25 +0300

    Add cash Map's

--- src/main/java/ru/iworking/personnel/reserve/dao/ProfFieldDao.java
+++ src/main/java/ru/iworking/personnel/reserve/dao/ProfFieldDao.java
@@ -5,32 +5,44 @@ import org.hibernate.Transaction;
 import ru.iworking.personnel.reserve.entity.ProfField;
 import ru.iworking.personnel.reserve.utils.HibernateUtil;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ProfFieldDao implements Dao<ProfField, Long> {
 
     private static volatile ProfFieldDao instance;
 
+    private Map<Long, ProfField> cashMap = null;
+
     @Override
     public List<ProfField> findAll() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-        List<ProfField> list = session.createQuery("FROM ProfField", ProfField.class).list();
-        session.flush();
-        transaction.commit();
-        session.close();
-        return list;
+        if (cashMap == null || cashMap.isEmpty()) {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            Transaction transaction = session.beginTransaction();
+            cashMap = session.createQuery("FROM ProfField", ProfField.class).list()
+                    .stream().collect(Collectors.toMap(ProfField::getId, Function.identity()));
+            session.flush();
+            transaction.commit();
+            session.close();
+        }
+        return cashMap.values().stream().collect(Collectors.toList());
     }
 
     @Override
     public ProfField find(Long id) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-        ProfField profField = session.get(ProfField.class, id);
-        session.flush();
-        transaction.commit();
-        session.close();
-        return profField;
+        if (cashMap == null) cashMap = new LinkedHashMap<>();
+        if (!cashMap.containsKey(id)) {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            Transaction transaction = session.beginTransaction();
+            cashMap.put(id, session.get(ProfField.class, id));
+            session.flush();
+            transaction.commit();
+            session.close();
+        }
+        return cashMap.get(id);
     }
 
     @Override
