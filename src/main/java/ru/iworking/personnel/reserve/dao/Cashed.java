13f9274591c385ed5088392dce26c0942f923c37
Алексей Земцов <zemtsov1997@yandex.ru>
2020-03-31 19:19:30 +0300

    fix methods in parent class

--- src/main/java/ru/iworking/personnel/reserve/dao/Cashed.java
+++ src/main/java/ru/iworking/personnel/reserve/dao/Cashed.java
@@ -4,15 +4,15 @@ import com.google.common.cache.LoadingCache;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
 public abstract class Cashed<O, I> {
 
     private static final Logger logger = LogManager.getLogger(Cashed.class);
 
     private LoadingCache<I, O> cash;
-
-    protected LoadingCache<I, O> getLoadingCache() {
-        return cash;
-    }
 
     public Cashed() {
         initCash();
@@ -33,6 +33,19 @@ public abstract class Cashed<O, I> {
      */
     public void initCashData(LoadingCache<I, O> cash) {
         logger.debug("initCashData is empty");
+    }
+
+    public O findFromCash(I id) {
+        try {
+            return cash.get(id);
+        } catch (ExecutionException e) {
+            logger.error(e);
+            return null;
+        }
+    }
+
+    public List<O> findAllFromCash() {
+        return cash.asMap().values().stream().collect(Collectors.toList());
     }
 
     public void clearCash() {
--- src/main/java/ru/iworking/personnel/reserve/dao/CashedDao.java
+++ src/main/java/ru/iworking/personnel/reserve/dao/CashedDao.java
@@ -1,27 +1,3 @@
 package ru.iworking.personnel.reserve.dao;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
-
-public abstract class CashedDao<O, I> extends Cashed<O, I> implements Dao<O, I> {
-
-    private static final Logger logger = LogManager.getLogger(Cashed.class);
-
-    public O findFromCash(I id) {
-        try {
-            return getLoadingCache().get(id);
-        } catch (ExecutionException e) {
-            logger.error(e);
-            return null;
-        }
-    }
-
-    public List<O> findAllFromCash() {
-        return getLoadingCache().asMap().values().stream().collect(Collectors.toList());
-    }
-
-}
+public abstract class CashedDao<O, I> extends Cashed<O, I> implements Dao<O, I> { }
