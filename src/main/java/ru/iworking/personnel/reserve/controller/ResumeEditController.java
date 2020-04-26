396698501e033a37fcd8e2452a993dd937d01163
Алексей Земцов <zemtsov1997@yandex.ru>
2020-04-26 21:56:00 +0300

    Create resume edit form

--- src/main/java/ru/iworking/personnel/reserve/controller/ResumeEditController.java
+++ src/main/java/ru/iworking/personnel/reserve/controller/ResumeEditController.java
@@ -1,6 +1,7 @@
 package ru.iworking.personnel.reserve.controller;
 
 import javafx.collections.FXCollections;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -12,11 +13,18 @@ import org.apache.logging.log4j.Logger;
 import ru.iworking.personnel.reserve.dao.*;
 import ru.iworking.personnel.reserve.entity.*;
 import ru.iworking.personnel.reserve.model.*;
+import ru.iworking.personnel.reserve.utils.ImageUtil;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class ResumeEditController extends FxmlController {
@@ -28,6 +36,8 @@ public class ResumeEditController extends FxmlController {
     private NumberPhoneFormatter numberPhoneFormatter = new NumberPhoneFormatter();
 
     @FXML private Pane resumePaneEdit;
+
+    @FXML private TextField resumeIdTextField;
 
     @FXML private TextField lastNameTextField;
     @FXML private TextField firstNameTextField;
@@ -96,6 +106,7 @@ public class ResumeEditController extends FxmlController {
     }
 
     public void setData(Resume resume) {
+        resumeIdTextField.setText(resume.getId().toString());
         lastNameTextField.setText(resume.getProfile().getLastName());
         firstNameTextField.setText(resume.getProfile().getFirstName());
         middleNameTextField.setText(resume.getProfile().getMiddleName());
@@ -130,6 +141,122 @@ public class ResumeEditController extends FxmlController {
     public void actionCancel(ActionEvent event) {
         hide();
         clear();
+    }
+
+    @FXML
+    public void actionSave(ActionEvent event) {
+        Boolean isSaved = save();
+        if (isSaved) {
+            hide();
+            clear();
+            getResumesTreeController().actionUpdate(event);
+        }
+    }
+
+    public Boolean save() {
+        if (isValid()) {
+            Long resumeId = null;
+            String vacancyIdStr = resumeIdTextField.getText();
+            if (vacancyIdStr != null && !vacancyIdStr.isEmpty()) {
+                resumeId = Long.valueOf(vacancyIdStr);
+            }
+
+            String lastNameStr = lastNameTextField.getText();
+            String firstNameStr = firstNameTextField.getText();
+            String middleNameStr = middleNameTextField.getText();
+            String numberStr = numberPhoneTextField.getText();
+            String emailStr = emailTextField.getText();
+            String professionStr = professionTextField.getText();
+            ProfField profField = profFieldComboBox.getValue();
+            String wageStr = wageTextField.getText();
+            Currency currency = currencyComboBox.getValue();
+            WorkType workType = workTypeComboBox.getValue();
+            ResumeState resumeState = resumeStateComboBox.getValue();
+            Education education = educationComboBox.getValue();
+            LocalDate expStart = experienceDateStartDatePicker.getValue();
+            LocalDate expEnd = experienceDateEndDatePicker.getValue();
+            String addressStr = addressTextArea.getText();
+
+            Resume resume = resumeId == null ? new Resume() : resumeDao.find(resumeId);
+            if (resume.getProfile() == null) resume.setProfile(new Profile());
+            resume.getProfile().setLastName(lastNameStr);
+            resume.getProfile().setFirstName(firstNameStr);
+            resume.getProfile().setMiddleName(middleNameStr);
+            if (resume.getNumberPhone() == null) resume.setNumberPhone(new NumberPhone());
+            resume.getNumberPhone().setNumber(numberStr);
+            resume.setEmail(emailStr);
+            resume.setProfession(professionStr);
+            if (profField != null) resume.setProfFieldId(profField.getId());
+            if (wageStr != null && !wageStr.isEmpty()) {
+                if (resume.getWage() == null) resume.setWage(new Wage());
+                try {
+                    resume.getWage().setCount(new BigDecimal(wageStr.replaceAll(",",".")));
+                    if (currency != null) resume.getWage().setCurrencyId(currency.getId());
+                } catch (Exception e) {
+                    logger.error(e);
+                }
+            }
+            if (workType != null) resume.setWorkTypeId(workType.getId());
+            if (resumeState != null) resume.setState(resumeState);
+            if (education != null) resume.setEducationId(education.getId());
+            if (resume.getExperience() == null) resume.setExperience(new Experience());
+            resume.getExperience().setDateStart(expStart);
+            resume.getExperience().setDateEnd(expEnd);
+            if (resume.getAddress() == null) resume.setAddress(new Address());
+            resume.getAddress().setHouse(addressStr);
+
+            Photo photo = null;
+
+            try(ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+                BufferedImage originalImage = SwingFXUtils.fromFXImage(photoImageView.getImage(), null);
+                ImageIO.write(originalImage, "png", stream);
+
+                photo = new Photo(ImageUtil.scaleToSize(stream.toByteArray(), null,200));
+            } catch (IOException e) {
+                logger.error(e);
+            }
+
+            if (photo != null) {
+                try {
+                    Long photoId = photoDao.createAndUpdateInCash(photo).getId();
+                    resume.setPhotoId(photoId);
+                } catch (OutOfMemoryError ex) {
+                    logger.error(ex);
+                }
+            }
+
+            if (resumeId == null) {
+                resumeDao.create(resume);
+            } else {
+                resumeDao.update(resume);
+            }
+            logger.debug("Created new resume: " + resume.toString());
+            return true;
+        } else {
+            logger.debug("Fields vacancy edit block is not valid...");
+            return false;
+        }
+    }
+
+    private Boolean isValid() {
+        Boolean isValid = true;
+        if (firstNameTextField.getText() == null || firstNameTextField.getText().length() <= 0) {
+            firstNameTextField.getStyleClass().add("has-error");
+            isValid = false;
+        }
+        if (lastNameTextField.getText() == null || lastNameTextField.getText().length() <= 0) {
+            lastNameTextField.getStyleClass().add("has-error");
+            isValid = false;
+        }
+        if (middleNameTextField.getText() == null || middleNameTextField.getText().length() <= 0) {
+            middleNameTextField.getStyleClass().add("has-error");
+            isValid = false;
+        }
+        if (professionTextField.getText() == null || professionTextField.getText().length() <= 0) {
+            professionTextField.getStyleClass().add("has-error");
+            isValid = false;
+        }
+        return isValid;
     }
 
     public void clear() {
@@ -168,6 +295,10 @@ public class ResumeEditController extends FxmlController {
     public void hide(AppFunctionalInterface function) {
         function.execute();
         this.hide();
+    }
+
+    public ResumesTreeController getResumesTreeController() {
+        return (ResumesTreeController) getControllerProvider().get(ResumesTreeController.class.getName());
     }
 
 }
